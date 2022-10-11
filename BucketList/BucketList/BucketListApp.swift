84390a8372718c08ef6a5b2953ff901cbd3b70bb
Michael Novosad <84763034+MichaelNovosad@users.noreply.github.com>
2022-10-11 18:08:55 +0300

    Add files via upload

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Michael Novosad on 08.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/BucketList/MKPointAnnotation-ObservableObject.swift
@@ -0,0 +1,27 @@
+//
+//  MKPointAnnotation-ObservableObject.swift
+//  BucketList
+//
+//  Created by Michael Novosad on 08.09.2021.
+//
+
+import MapKit
+
+extension MKPointAnnotation: ObservableObject {
+    public var wrappedTitle: String {
+        get {
+            self.title ?? "Unknown value"
+        }
+        set {
+            title = newValue
+        }
+    }
+    public var wrappedSubtitle: String {
+        get {
+            self.subtitle ?? "Unknown value"
+        }
+        set {
+            subtitle = newValue
+        }
+    }
+}
